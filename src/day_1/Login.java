aad9ef3abaa0e7a6eb3f7295d47122647bb8f60b
Alimjan Abliz <62036835+Alimjan921@users.noreply.github.com>
2020-06-20 18:42:35 +0200

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -5,5 +5,9 @@ public class Login {
     public static void main(String[] args) {
 
         System.out.println("as user i should able to login with valide credentials");
+        
+        //Brand new changes from GitHub
+        
+        
     }
 }
