0777f6aab2956b807ad87b010d42936840ffd59d
Alimjan921 <attila0921@hotmail.com>
2020-06-20 18:41:23 +0200

    add Login.java

---
+++ src/day_1/Login.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("as user i should able to login with valide credentials");
+    }
+}
