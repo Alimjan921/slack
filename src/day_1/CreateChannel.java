f53cc281a20303c7b8753afe9ceb04f516ee89ea
Alimjan921 <attila0921@hotmail.com>
2020-06-20 18:28:48 +0200

    add CreateChannel.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class CreateChannel {
+
+    public static void main(String[] args) {
+
+        System.out.println("as user it should be able to Create Channal");
+    }
+}
